8f4ca708b706a0ecd55bc23ebbbb44e81fb92768
mas <m@stdwtr.io>
2023-12-04 10:45:12 -0500

    Change Number to Span

--- src/ch03.rs
+++ src/ch03.rs
@@ -1,26 +1,30 @@
 fn get_data() -> Vec<String> {
-    include_str!("ch03.txt").lines().map(|l| l.to_string()).collect()
+    include_str!("ch03.txt")
+        .lines()
+        .map(|l| l.to_string())
+        .collect()
 }
 
-struct Number {
+struct Span {
     row: usize,
     start: usize,
     end: usize,
-    num: i32,
+    contents: String,
 }
 
-impl Number {
+impl Span {
     fn check_row(&self, row: i32, schematic: &Vec<String>) -> bool {
-        if row < 0 || row as usize >= schematic.len() { return false; }
+        if row < 0 || row as usize >= schematic.len() {
+            return false;
+        }
         let start = (self.start as i32 - 1).max(0);
         let end = (self.end as i32 + 1).min(schematic[0].len() as i32 - 1);
         for i in start..end {
-            let msg = format!("start: {}, line: {}, num: {}",
-                              start, &schematic[row as usize], self.num);
-            let c = schematic[row as usize]
-                .chars()
-                .nth(i as usize)
-                .expect(&msg);
+            let msg = format!(
+                "start: {}, line: {}, num: {}",
+                start, &schematic[row as usize], self.contents
+            );
+            let c = schematic[row as usize].chars().nth(i as usize).expect(&msg);
             if !c.is_alphanumeric() && c != '.' {
                 return true;
             }
@@ -32,7 +36,6 @@ impl Number {
         self.check_row(self.row as i32 - 1, schematic)
             || self.check_row(self.row as i32 + 1, schematic)
             || self.check_row(self.row as i32, schematic)
-
     }
 }
 
@@ -47,20 +50,26 @@ fn ch03_1() -> i32 {
                 .chars()
                 .take_while(|c| c.is_numeric())
                 .collect::<String>();
-            let n = Number {
+            let n = Span {
                 row: i,
                 start: j,
-                end: j+num_str.len(),
-                num: num_str.parse().expect("Bad date 🐵")
+                end: j + num_str.len(),
+                contents: num_str,
             };
-            j=n.end;
-            total += if n.touches_symbol(&data) { n.num } else { 0 };
+            j = n.end;
+            total += if n.touches_symbol(&data) {
+                n.contents.parse().expect("Bad number!")
+            } else {
+                0
+            };
         }
     }
     total
 }
 
-fn ch03_2() -> i32 { 0 }
+fn ch03_2() -> i32 {
+    0
+}
 
 pub fn ch03() {
     println!("03-1: {}", ch03_1());
