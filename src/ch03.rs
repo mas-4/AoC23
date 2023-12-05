8cc2e376c1fc16a2e981e5889340fb2b162254c2
Michael Sendker <m@standingwater.io>
2023-12-05 07:30:50 -0500

    Wrong answer, not working

--- src/ch03.rs
+++ src/ch03.rs
@@ -39,8 +39,64 @@ impl Span {
             || self.check_row(self.row as i32, schematic, closure)
     }
 
-    fn gear_ratio(&self) -> i32 {
-        2
+    fn get_num(schematic: &[String], row: i32, col: i32) -> i32 {
+        let c = schematic[row as usize].chars().nth(col as usize).expect("Bad char");
+        // number can spread either left or right
+        if c.is_numeric() {
+            let mut i = col;
+            while i >= 0 { // go left
+                let c = schematic[row as usize].chars().nth(i as usize).expect("Bad char left");
+                if !c.is_numeric() || c == '.' {
+                    i += 1; // traced too far
+                    break;
+                }
+                i -= 1;
+            }
+            i = i.max(0);
+            let mut j = col;
+            while j < schematic[0].len() as i32 { // go right
+                let c = schematic[row as usize].chars().nth(j as usize).expect("Bad char right");
+                if !c.is_numeric() || c == '.' {
+                    j -= 1; // traced too far
+                    break;
+                }
+                j += 1;
+            }
+            let num_str = schematic[row as usize].chars().skip(i as usize).take((j - i + 1) as usize).collect::<String>();
+            let msg = format!("bad number num_str: {}, row: {}, col: {}\n{}", num_str, row, col, schematic[row as usize]);
+            let msg = msg.as_str();
+            num_str.parse().expect(msg)
+        }
+        else {
+            0
+        }
+    }
+
+    fn gear_ratio(&self, schematic: &Vec<String>) -> i32 {
+        let mut nums: Vec<i32> = Vec::new();
+        let i0 = (self.row as i32 - 1).max(0);
+        let i1 = (self.row as i32 + 1).min(schematic.len() as i32 - 1);
+        let j0 = (self.start as i32 - 1).max(0);
+        let j1 = (self.end as i32 + 1).min(schematic[0].len() as i32 - 1);
+
+        for i in i0..i1 {
+            for j in j0..j1 {
+                let c = schematic[i as usize].chars().nth(j as usize).expect("Bad char");
+                if c.is_numeric() {
+                    let num = Span::get_num(schematic, i, j);
+                    if nums.contains(&num)
+                    {
+                        continue
+                    }
+                    nums.push(num);
+                }
+                if nums.len() == 2 {
+                    println!("nums: {:?}", nums);
+                    return nums[0] * nums[1]
+                }
+            }
+        }
+        0
     }
 }
 
@@ -89,7 +145,7 @@ fn ch03_2() -> i32 {
             j += 1;
             let closure = |c: char| c.is_numeric();
             total += if n.touches_closure(&data, closure) {
-                n.gear_ratio()
+                n.gear_ratio(&data)
             } else {
                 0
             }
