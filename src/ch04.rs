ef7d8c2fac7fab8a6333f7a4cfcb5064dce74c9a
Michael Sendker <m@standingwater.io>
2023-12-07 16:59:40 -0500

    Solve chapter 4

--- src/ch04.rs
+++ src/ch04.rs
@@ -7,7 +7,6 @@ fn get_data() -> Vec<String> {
 
 #[derive(Debug)]
 struct Card {
-    card_no: i32,
     winning_numbers: Vec<i32>,
     your_numbers: Vec<i32>,
 }
@@ -15,12 +14,6 @@ struct Card {
 impl Card {
     fn new(line: &str) -> Card {
         let parts: Vec<&str> = line.split(':').collect();
-        let card_no = parts[0]
-            .split_whitespace()
-            .nth(1)
-            .expect("Bad card no")
-            .parse::<i32>()
-            .expect("Bad card no");
         let numbers: Vec<&str> = parts[1]
             .split('|')
             .collect();
@@ -33,10 +26,19 @@ impl Card {
             .map(|n| n.trim().parse::<i32>().expect("Bad your number"))
             .collect();
         Card {
-            card_no,
             winning_numbers,
             your_numbers,
         }
+    }
+
+    fn count_winning_numbers(&self) -> usize {
+        let mut count = 0;
+        for n in &self.your_numbers {
+            if self.winning_numbers.contains(n) {
+                count += 1;
+            }
+        }
+        count
     }
 
     fn score(&self) -> i32 {
@@ -63,7 +65,21 @@ fn ch04_1() -> i32 {
 }
 
 fn ch04_2() -> i32 {
-    0
+    let cards = get_data()
+        .iter()
+        .map(|l| Card::new(l.as_str()))
+        .collect::<Vec<Card>>();
+    let mut card_counts = vec![1; cards.len()];
+    for (i, c) in cards.iter().enumerate() {
+        let winning_count = c.count_winning_numbers();
+        if winning_count == 0 {
+            continue;
+        }
+        for j in 1..(winning_count+1) {
+            card_counts[i + j] += 1*card_counts[i];
+        }
+    }
+    card_counts.iter().sum()
 }
 
 pub fn ch04() {
