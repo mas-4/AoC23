694fa953ea292908705d1dc4722db08ac016f660
mas <m@stdwtr.io>
2023-12-16 15:06:59 -0500

    Refactor

--- src/ch05.rs
+++ src/ch05.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 fn get_data() -> Almanac {
     let lines = include_str!("ch05.txt")
         .lines()
@@ -13,62 +11,98 @@ fn get_data() -> Almanac {
         .map(|s| s.parse::<usize>().expect("Not an integer!"))
         .collect();
 
-    let mut source = String::new();
-    let mut maps = HashMap::new();
-    let mut map_order = Vec::new();
+    let mut parse_lines = Vec::new();
+    let mut maps: Vec<Map> = Vec::new();
     for line in lines.iter().skip(2) {
-        if line.trim() == "" {
+        if line.trim().is_empty() {
+            maps.push(Map::new(&parse_lines));
+            parse_lines.clear();
             continue;
-        } else if line.chars().next().expect("No string!").is_alphabetic() {
-            source = line.split('-').collect::<Vec<&str>>()[0].to_string();
-            maps.insert(source.clone(), Vec::new());
-            map_order.push(source.clone());
         } else {
-            // Insert a new map into the vector
-            maps.get_mut(&source).expect("No source!").push(Map::new(line));
+            parse_lines.push(line);
         }
     }
+    maps.push(Map::new(&parse_lines));
+
     Almanac {
         maps,
-        map_order,
         seeds,
     }
 }
 
+fn parse_map_title(line: &String) ->  (String, String) {
+    // Parses the title of a map, which is of the form
+    // "source-to-destination map:"
+    let parts: Vec<&str> = line.split("-to-").collect();
+    let mut destination = parts[1].trim();
+    if let Some(index) = destination.find(" map") {
+        destination = &destination[..index];
+    }
+    (parts[0].trim().to_string(), destination.to_string())
+}
+
+
+#[derive(Debug)]
 struct Almanac {
-    maps: HashMap<String, Vec<Map>>,
-    map_order: Vec<String>,
+    maps: Vec<Map>,
     seeds: Vec<usize>,
 }
-
-impl Almanac {
-    fn feed_forward(&self, seed: usize) -> usize {
-        let mut result = seed;
-        for map in self.map_order.iter() {
-            for m in self.maps.get(map).expect("No map!") {
-                if m.contains(result) {
-                    result = m.translate(result);
-                    break;
-                }
-            }
-        }
-        result
-    }
-}
-
+#[derive(Debug)]
 struct Map {
+    source: String,
+    destination: String,
+    translations: Vec<Translation>,
+}
+#[derive(Debug)]
+struct Translation {
     source_start: usize,
     destination_start: usize,
     range: usize,
 }
 
+
+impl Almanac {
+    fn feed_forward(&self, seed: usize) -> usize {
+        let mut result = seed;
+        for map in self.maps.iter() {
+            result = map.translate(result);
+        }
+        result
+    }
+}
+
 impl Map {
-    fn new(line: &String) -> Map {
+    fn new(lines: &Vec<&String>) -> Map {
+        let (source, destination) = parse_map_title(lines[0]);
+        let translations = lines
+            .iter()
+            .skip(1)
+            .map(|line| Translation::new(line))
+            .collect();
+        Map {
+            source,
+            destination,
+            translations,
+        }
+    }
+
+    fn translate(&self, n: usize) -> usize {
+        for translation in self.translations.iter() {
+            if translation.contains(n) {
+                return translation.translate(n);
+            }
+        }
+        n
+    }
+}
+
+impl Translation {
+    fn new(line: &String) -> Translation {
         let nums: Vec<usize> = line
             .split_whitespace()
             .map(|n| n.parse::<usize>().expect("bad integer"))
             .collect();
-        Map {
+        Translation {
             source_start: nums[1],
             destination_start: nums[0],
             range: nums[2],
@@ -76,17 +110,18 @@ impl Map {
     }
 
     fn contains(&self, n: usize) -> bool {
-        n >= self.source_start && n < self.source_start + self.range
+        n >= self.source_start && n <= self.source_start + self.range
     }
     fn translate(&self, n: usize) -> usize {
         n - self.source_start + self.destination_start
     }
-
-
 }
 
 fn ch05_1() -> usize {
     let almanac = get_data();
+    for map in almanac.maps.iter() {
+        println!("{:?}", map.source);
+    }
     almanac
         .seeds
         .iter()
@@ -96,7 +131,13 @@ fn ch05_1() -> usize {
 }
 
 fn ch05_2() -> usize {
-    0
+    let almanac = get_data();
+    let seed_pairs = almanac
+        .seeds
+        .chunks(2);
+    3
+
+
 }
 
 pub fn ch05() {
